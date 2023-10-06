a91b6a3048bbf76a3c53a5ce9ea1c31b3e3148f7
mathoks <mathoks2012@gmail.com>
2023-10-06 09:03:35 +0100

    new task on players

--- Product/product/src/App.js
+++ Product/product/src/App.js
@@ -5,36 +5,19 @@ import Image from "./components/image";
 import Price from "./components/price";
 import Name from "./components/name";
 import Description from "./components/description";
+import { PlayerList } from "./components/playerList";
 
 function App(props) {
-  const firstName = props.firstName || "there";
   return (
     <>
       <Container style={{display: 'flex'}} >
         <Col lg={3}></Col>
         <Col lg={6}>
           <header style={{paddingBottom: '25px'}}>
-            <h4>My Product Card</h4>
+            <h4>My Player Card</h4>
           </header>
           <section>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" as= 'img' src= {Image} height={200} width={50}/>
-              <Card.Body>
-                <Card.Title>My Product</Card.Title>
-                <Card.Text>
-                  <p>
-                    Name: <Name />
-                  </p>
-                  <p>
-                    Price: <Price />
-                  </p>
-                  <p>
-                    Description: <Description />
-                  </p>
-                </Card.Text>
-              </Card.Body>
-            </Card>
-            <p>hello {firstName}!</p>
+          <PlayerList/>
           </section>
         </Col>
         <Col lg={3}></Col>
--- Product/product/src/product.js
+++ Product/product/src/product.js
@@ -1,7 +1,34 @@
 import pic from "./assets/shoe.jpg";
-export const Product = {
-    'name': 'shoes',
-    'price': 1500,
-    'description': 'Running shoes',
-    'path': pic
+export const Product = [{
+    'name': 'Rooney',
+    'age': 24,
+    'Nationality': 'England',
+    'image': 'https://media.gettyimages.com/id/495511132/photo/manchester-united-fc-v-pfc-cska-moskva-uefa-champions-league.jpg?s=612x612&w=gi&k=20&c=-4r0Bvj-wpSZdUOLcH4ZSXhNIufqkRFMiAk24q05SPQ=',
+    "jesey": "7",
+    "club": "Manchester United"
+},
+{
+    'name': 'Neymar',
+    'age': 20,
+    'Nationality': 'Brazil',
+    'image': 'https://media.gettyimages.com/id/1466409940/photo/paris-saint-germain-v-fc-bayern-m%C3%BCnchen-round-of-16-leg-one-uefa-champions-league.jpg?s=612x612&w=gi&k=20&c=cbSdrwPn-INmLZKLdWxt2W2X8FUweRuAT5sVwWvdYtM=',
+    "jesey": "10",
+    "club": "Paris Saint-Germain"
+},
+{
+    'name': 'Messi',
+    'age': 23,
+    'Nationality': 'Argentina',
+    'image': 'https://th.bing.com/th/id/R.a998bf6d846d03328d0cfb87c8ce0916?rik=g%2bIJxWcmLlYIsg&pid=ImgRaw&r=0',
+    "jesey": "10",
+    "club": "Barcelona"
+},
+{
+    'name': 'Ronaldo',
+    'age': 25,
+    'Nationality': 'Portugal',
+    'image': 'https://media.gettyimages.com/id/1358485714/photo/norwich-city-v-manchester-united-premier-league.jpg?s=612x612&w=gi&k=20&c=-wrOR7NSzfX_SJIKgvTpbq_rhUgO69kShMT0gS3ygHA=',
+    "jesey": "7",
+    "club": "Real Madrid"
 }
+]
